42971b9d55059b3c6b8037d2b623507d601ed5b1
Tim MacDonald <hello@timacdonald.me>
2024-05-06 16:57:36 +1000

    format slow queries config

--- config/pulse.php
+++ config/pulse.php
@@ -193,10 +193,7 @@ return [
         Recorders\SlowQueries::class => [
             'enabled' => env('PULSE_SLOW_QUERIES_ENABLED', true),
             'sample_rate' => env('PULSE_SLOW_QUERIES_SAMPLE_RATE', 1),
-            'threshold' => [
-                'default' => env('PULSE_SLOW_QUERIES_THRESHOLD', 1000),
-                // '/["`]slow_table["`]/' => 1500,
-            ],
+            'threshold' => env('PULSE_SLOW_QUERIES_THRESHOLD', 1000),
             'location' => env('PULSE_SLOW_QUERIES_LOCATION', true),
             'max_query_length' => env('PULSE_SLOW_QUERIES_MAX_QUERY_LENGTH', null),
             'ignore' => [
